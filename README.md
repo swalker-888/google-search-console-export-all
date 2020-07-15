8808654c5c2990f435e8bbe0089f3dd30c15cbf9
stephen walker <steve.walker@journeyfurther.com>
2020-07-15 16:24:16 +0100

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# GSC Export All Data
+
+Use this script to export all your Google SEO data including URLs, queries, clicks, impressions and rankings.
+
+The Google Search Console interface is limited to exports of 1000 rows, the sample Google API is limited to 25,000 rows - this script exports ALL data.
---
+++ index.js
@@ -0,0 +1,80 @@
+const path = require("path");
+const { google } = require("googleapis");
+const { authenticate } = require("@google-cloud/local-auth");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+
+const webmasters = google.webmasters("v3");
+
+const siteUrl = "https://www.evanscycles.com";
+const startDate = "2020-07-01";
+const endDate = "2020-07-15";
+
+async function collectAllGSCData(siteUrl, startDate, endDate) {
+  //   Obtain user credentials to use for the request
+  const auth = await authenticate({
+    keyfilePath: path.join(__dirname, "./oauth2.keys.json"),
+    scopes: [
+      "https://www.googleapis.com/auth/webmasters",
+      "https://www.googleapis.com/auth/webmasters.readonly",
+    ],
+  });
+  google.options({ auth });
+
+  const maxRows = 25000;
+  let i = 0;
+  const rows = [];
+  let rowCheck = true;
+
+  do {
+    let loopStartRow = i * maxRows;
+    const res = await webmasters.searchanalytics.query({
+      siteUrl,
+      requestBody: {
+        startDate,
+        endDate,
+        dimensions: ["date", "page", "query"],
+        rowLimit: maxRows,
+        startRow: loopStartRow,
+      },
+    });
+    if (res.data.rows) {
+      res.data.rows.map((row) => {
+        let data = {};
+        data.date = row.keys[0];
+        data.url = row.keys[1];
+        data.query = row.keys[2];
+        data.clicks = row.clicks;
+        data.impressions = row.impressions;
+        data.ctr = row.ctr;
+        data.position = row.position;
+        rows.push(data);
+      });
+    } else {
+      rowCheck = false;
+    }
+    console.log(`Google API Search Console Request #${i}`);
+    i = i + 1;
+  } while (rowCheck);
+
+  const csvWriter = createCsvWriter({
+    path: "gsc-export.csv",
+    header: [
+      { id: "date", title: "Date" },
+      { id: "url", title: "URL" },
+      { id: "query", title: "Query" },
+      { id: "clicks", title: "Clicks" },
+      { id: "impressions", title: "Impressions" },
+      { id: "ctr", title: "CTR" },
+      { id: "position", title: "Position" },
+    ],
+  });
+
+  csvWriter
+    .writeRecords(rows)
+    .then(() => console.log("GSC API requests complete"));
+}
+
+if (module === require.main) {
+  collectAllGSCData(siteUrl, startDate, endDate).catch(console.error);
+}
+module.exports = runSample;
